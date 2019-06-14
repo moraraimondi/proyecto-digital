56a20149b7afbb67773ad9995223c1d22f20e439
mora <moraraimondi@gmail.com>
2019-06-14 11:07:55 -0300

    carousel terminado

--- proyecto1.js
+++ proyecto1.js
@@ -5,8 +5,9 @@ fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=2acc6c4b1703a31715d20
 })
 .then(function(data){
   var imagen = document.querySelectorAll(".imagenes-populares")
+  console.log(imagen);
   var srcImage = "https://image.tmdb.org/t/p/w500"
-  for (var i =0; i<6; i++){
+  for (var i =0; i<10; i++){
     imagen[i].setAttribute("src", (srcImage + data.results[i].poster_path))
   }
 })
@@ -14,16 +15,6 @@ fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=2acc6c4b1703a31715d20
   console.log("The error was: " + error);
 })
 
+//MEJOR PUNTAJE (HOME)
 
 //GENEROS (LISTA)
-fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=2acc6c4b1703a31715d2049b8767b9ff&language=en-US")
-.then(function(response){
-  return.response.json()
-})
-.then(function(informacion){
-  var generos = informacion.results
-  console.log(generos);
-  for (var i = 0; i < generos.length; i++) {
-    console.log(generos[i]);
-  }
-})
