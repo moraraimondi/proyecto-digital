d0706a1ad38612862893c773c53075ea0a7b0fc0
Valu <vsocincasas@udesa.edu.ar>
2019-06-14 11:27:45 -0300

    Arreglos

--- proyecto1.js
+++ proyecto1.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 localStorage.removeItem("usuario")
 window.addEventListener("load", function() {
@@ -6,20 +6,8 @@ window.addEventListener("load", function() {
   if (localStorage.getItem("usuario") != null) {
     var nombreDeUsuario = localStorage.getItem("usuario")
     document.querySelector(".espacio-login").innerHTML = "Bienvenide " + nombreDeUsuario
-=======
-//ESTRENOS (HOME)
-fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=2acc6c4b1703a31715d2049b8767b9ff&language=en-US&page=1")
-.then(function(response){
-  return response.json();
-})
-.then(function(data){
-  var imagen = document.querySelectorAll(".imagenes-populares")
-  console.log(imagen);
-  var srcImage = "https://image.tmdb.org/t/p/w500"
-  for (var i =0; i<10; i++){
-    imagen[i].setAttribute("src", (srcImage + data.results[i].poster_path))
->>>>>>> master
   }
+//ESTRENOS (HOME)
 
   //ESTRENOS (HOME)
   fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=2acc6c4b1703a31715d2049b8767b9ff&language=en-US&page=1")
