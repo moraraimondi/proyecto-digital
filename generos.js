d6615c5d0a0ddb1fa85d8e5ef5de91e38dbc2b4f
Valu <vsocincasas@udesa.edu.ar>
2019-06-14 12:32:01 -0300

    generos lista

--- generos.js
+++ generos.js
@@ -1,11 +1,12 @@
-fetch(url)
+
+fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=2acc6c4b1703a31715d2049b8767b9ff&language=en-US")
 .then(function(response){
   return response.json();
 })
 .then(function(data){
-
+  console.log(data)
   for (var i =0; i<data.genres.length; i++){
-    imagen[i].setAttribute("src", (srcImage + data.results[i].poster_path))
+    document.querySelector("ul.generosLista").innerHTML+= "<a href=detalle.html id=i>"+ "<li>"  + data.genres[i].name + "<li>"+ "</a>"
   }
 })
 .catch(function(error){
