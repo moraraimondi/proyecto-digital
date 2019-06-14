e2891cd32bdeac6f25eaaadfbef3cf5fee346e8a
mora <moraraimondi@gmail.com>
2019-06-14 09:22:04 -0300

    api generos incompleto

--- proyecto1.js
+++ proyecto1.js
@@ -13,3 +13,17 @@ fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=2acc6c4b1703a31715d20
 .catch(function(error){
   console.log("The error was: " + error);
 })
+
+
+//GENEROS (LISTA)
+fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=2acc6c4b1703a31715d2049b8767b9ff&language=en-US")
+.then(function(response){
+  return.response.json()
+})
+.then(function(informacion){
+  var generos = informacion.results
+  console.log(generos);
+  for (var i = 0; i < generos.length; i++) {
+    console.log(generos[i]);
+  }
+})
