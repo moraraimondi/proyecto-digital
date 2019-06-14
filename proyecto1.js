0fb330274aa5ff685f6013593639e38061666ddd
mora <moraraimondi@gmail.com>
2019-06-14 12:27:16 -0300

    imagenes del home

--- proyecto1.js
+++ proyecto1.js
@@ -1,14 +1,19 @@
-
-
+//LOGIN
 localStorage.removeItem("usuario")
 window.addEventListener("load", function() {
+  document.querySelector("form.login").onsubmit = function(e) {
+    e.preventDefault()
 
+    var nombreDeUsuario = document.querySelector(".nombreDeUsuario").value
+    document.querySelector(".espacio-login").innerHTML = "Bienvenido/a " + nombreDeUsuario + "!"
+    document.querySelector(".uk-modal-close-default").click()
+    localStorage.setItem("usuario", nombreDeUsuario)
+  }
+})
   if (localStorage.getItem("usuario") != null) {
     var nombreDeUsuario = localStorage.getItem("usuario")
     document.querySelector(".espacio-login").innerHTML = "Bienvenide " + nombreDeUsuario
   }
-//ESTRENOS (HOME)
-
   //ESTRENOS (HOME)
   fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=2acc6c4b1703a31715d2049b8767b9ff&language=en-US&page=1")
   .then(function(response){
@@ -25,19 +30,34 @@ window.addEventListener("load", function() {
     console.log("The error was: " + error);
   })
 
-  document.querySelector("form.login").onsubmit = function(e) {
-    e.preventDefault()
+  //LISTA MEJOR RATING
+  fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=2acc6c4b1703a31715d2049b8767b9ff&language=en-US&page=1")
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(data){
+    var imagen = document.querySelectorAll(".ratingP")
+    var srcImage = "https://image.tmdb.org/t/p/w500"
+    for (var i =0; i<6; i++){
+      imagen[i].setAttribute("src", (srcImage + data.results[i].poster_path))
+    }
+  })
+  .catch(function(error){
+    console.log("The error was: " + error);
+  })
 
-    var nombreDeUsuario = document.querySelector(".nombreDeUsuario").value
-    document.querySelector(".espacio-login").innerHTML = "Bienvenido/a " + nombreDeUsuario + "!"
-
-    document.querySelector(".uk-modal-close-default").click()
-
-    localStorage.setItem("usuario", nombreDeUsuario)
-  }
-
-})
-
-//MEJOR PUNTAJE (HOME)
-
-//GENEROS (LISTA)
+  //LISTA POPULARES
+  fetch("https://api.themoviedb.org/3/movie/popular?api_key=2acc6c4b1703a31715d2049b8767b9ff&language=en-US&page=1")
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(data){
+    var imagen = document.querySelectorAll(".popularesP")
+    var srcImage = "https://image.tmdb.org/t/p/w500"
+    for (var i =0; i<6; i++){
+      imagen[i].setAttribute("src", (srcImage + data.results[i].poster_path))
+    }
+  })
+  .catch(function(error){
+    console.log("The error was: " + error);
+  })
