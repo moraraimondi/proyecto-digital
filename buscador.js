efe9a67ee15025a6d41513f0cc7a4f366fe0bba0
Valu <vsocincasas@udesa.edu.ar>
2019-06-19 16:49:59 -0300

    muchas cosas

--- buscador.js
+++ buscador.js
@@ -1,32 +1,39 @@
 window.addEventListener("load", function(){
+
   var urlSearchParams = new URLSearchParams(window.location.search)
-  var textoBuscado= urlSearchParams.get("buscador")
-  console.log(textoBuscado);
+  var buscador = urlSearchParams.get('buscador')
+  console.log(buscador);
 
-  fetch("https://api.themoviedb.org/3/search/movie?api_key=2acc6c4b1703a31715d2049b8767b9ff&language=en-US&page=1&include_adult=false")
-  .then(function (respuesta){
-    return respuesta.json()
-  })
-  .then(function(información){
-    console.log(información);
-    var arrayBuscador = información.results
-    var imagen = document.querySelector(".buscar")
-    var texto = document.querySelector(".buscar-titulo")
-    for (var i=0; i < informacion.results.length; i++){
-    imagen[i].setAttribute("src", (posterUrl + información.results[i].poster_path))
-    for (var i = 0; i <6; i++) {
-      texto[i].innerHTML += información.results[i].title
-    }
-    console.log(informacion.results[i].id)
-    titulo =informacion.results[i].title
-    poster= informacion.results.poster_path
-    posterUrl= "https://image.tmdm.org/t/p/original/"
-    image= posterUrl + poster
-    console.log(image);
-  }
-  })
-  .catch(function(error){
-    console.log("Error:" + error);
+  var API_KEY = "928ad4dee3a02646fa1725b8bcaa2a96"
+  var url = "https://api.themoviedb.org/3/search/movie?api_key="+API_KEY+"&language=en-US&query="+buscador+"&page=1&include_adult=false"
+  fetch(url)
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(objetoLiteralRespuesta) {
+      console.log(objetoLiteralRespuesta);
+      //GUARDO EL ARRAY DE PELIS
+      var arrayDePeliculas = objetoLiteralRespuesta.results
+      // CAPTURO EL UL
+      var ul = document.querySelector('section ul')
 
-  })
+      var li = ""
+      // PARTE FIJA DE LA URL DE LA IMAGEN
+      var urlImg = "https://image.tmdb.org/t/p/original"
+      // RECORRO EL ARRAY DE PELIS
+      for (var i = 0; i < arrayDePeliculas.length; i++) {
+          li = "<li>"
+          li +="<a href='detalle.html?idPelicula="+arrayDePeliculas[i].id+"'>"
+          li +=   "<p>"+arrayDePeliculas[i].title+"</p>"
+          li +=   "<img src='"+urlImg + arrayDePeliculas[i].poster_path+"' style='width:300px;'>"
+          li +="</a>"
+          li += "</li>"
+
+          ul.innerHTML += li
+      }
+    })
+    .catch(function(error) {
+      console.log("the error was: " + error);
+    })
+
 })
--- listado.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("load", function(){
-  var url string = window.locatios.href;
-  var url = new URL (url)
-}
-)
